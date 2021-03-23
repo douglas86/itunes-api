9a07bc72b65fd92159579f7da810ed96591a786f
douglas86 <douglasmaxton@gmail.com>
2021-03-23 15:33:36 +0200

    Used the itunes api to search for jack johnson

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-import express from 'express'
+import express from "express";
+import axios from "axios";
 
-let app = express()
+let app = express();
+
+const getBreeds = async () => {
+  try {
+    return await axios.get(
+      `https://itunes.apple.com/search?term=jack+johnson&limit=1&entity=musicVideo`
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const countBreeds = async () => {
+  const breeds = await getBreeds();
+
+  console.log(breeds.data.results);
+};
 
 app.get(`/`, (req, res) => {
-	res.send(`Hello World`)
-})
+  res.send(countBreeds());
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-	console.log(`Server is listening on port ${PORT}`)
-})
+  console.log(`Server is listening on port ${PORT}`);
+});
